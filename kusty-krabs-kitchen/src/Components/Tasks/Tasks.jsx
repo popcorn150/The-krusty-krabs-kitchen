7dc9e97dd986397da12fbc121e13fdf5d8b1d4e4
popcorn150 <hungrymanchannel@gmail.com>
2024-07-27 22:56:32 +0100

    earn page done

--- kusty-krabs-kitchen/src/Components/Tasks/Tasks.jsx
+++ kusty-krabs-kitchen/src/Components/Tasks/Tasks.jsx
@@ -5,16 +5,16 @@ function TaskCard({ svgIcon1, title, subtitle, svgIcon2 }) {
     return (
         <div className='task__container'>
             <div className='task__contents'>
-                <div dangerouslySetInnerHTML={{ __html: svgIcon1 }} className='task__style' />
+                <div dangerouslySetInnerHTML={{ __html: svgIcon1 }} />
                 <div className='task__text'>
                     <h3 className='task__title'>{title}</h3>
                     <div className='task__subtitle-container'>
-                        <div dangerouslySetInnerHTML={{ __html: svgIcon2 }} className='task__icon' />
+                        <img src="/coin.png" alt="coin icon" className='task__icon' />
                         <h4 className='task__subtitle'>{subtitle}</h4>
                     </div>
                 </div>
                 <svg width="80px" height="80px" fill="none" stroke="#1c1f24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path d="m10 7 5 5-5 5" stroke="#85827d" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"></path>
+                    <path d="m10 7 5 5-5 5" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"></path>
                 </svg>
             </div>
         </div>
