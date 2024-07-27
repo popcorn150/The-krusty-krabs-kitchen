e37cc855dd73e6cf5322aa942244992ec35345f0
popcorn150 <hungrymanchannel@gmail.com>
2024-07-27 20:21:22 +0100

    today's commit

--- kusty-krabs-kitchen/src/Components/Tasks/Tasks.jsx
+++ kusty-krabs-kitchen/src/Components/Tasks/Tasks.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import './Tasks.css';
 
-function TaskCard({ key, svgIcon1, title, subtitle, svgIcon2, coinIcon }) {
+function TaskCard({ svgIcon1, title, subtitle, svgIcon2 }) {
     return (
-        <>
-            <div className='task__container'>
-                <div className='task__contents'>
-                    <div dangerouslySetInnerHTML={{ __html: svgIcon1 }} className='task__style' />
-                    <div>
-                    <h3 dangerouslySetInnerHTML={{__html: title}}/>
-                    <div dangerouslySetInnerHTML={{__html: coinIcon}}/>
-                    <h5 dangerouslySetInnerHTML={{__html: subtitle}} />
+        <div className='task__container'>
+            <div className='task__contents'>
+                <div dangerouslySetInnerHTML={{ __html: svgIcon1 }} className='task__style' />
+                <div className='task__text'>
+                    <h3 className='task__title'>{title}</h3>
+                    <div className='task__subtitle-container'>
+                        <div dangerouslySetInnerHTML={{ __html: svgIcon2 }} className='task__icon' />
+                        <h4 className='task__subtitle'>{subtitle}</h4>
                     </div>
-                    <div dangerouslySetInnerHTML={{ __html: svgIcon2 }} />
                 </div>
+                <svg width="80px" height="80px" fill="none" stroke="#1c1f24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path d="m10 7 5 5-5 5" stroke="#85827d" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"></path>
+                </svg>
             </div>
-        </>
+        </div>
     )
 }
 
