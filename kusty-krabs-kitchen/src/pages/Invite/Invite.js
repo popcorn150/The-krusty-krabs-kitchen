839161ab91c43d79e84a160f7940a9e1814844ed
popcorn150 <hungrymanchannel@gmail.com>
2024-07-29 17:15:11 +0100

    invite page done

--- kusty-krabs-kitchen/src/pages/Invite/Invite.js
+++ kusty-krabs-kitchen/src/pages/Invite/Invite.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import InviteCard from '../../Components/invite/invite';
 import './Invite.css';
+import Tasks from '../../Components/Tasks/Tasks';
+
 
 
 const inviteIcon = `<img src="/coin.png" alt="invite" class="invite__style"/>`;
 const telegramIcon = `<img src="/telegramPremium.png" alt="telegram" class="telegram__style" />`;
 
+
 export default function Invite() {
+
+    const [isVisible, setIsVisible] = useState(false);
+
+    const handleCopyClick = () => {
+        setIsVisible(!isVisible);
+        setTimeout(() => {
+            setIsVisible(false);
+        }, 2000);
+    };
+
     return (
         <>
-                <h3 className='mainTitle'>Invite a friend and get bonuses</h3>
+            <div className={`hidden-div ${isVisible ? 'visible' : ''}`}>
+                Invite copied.....
+            </div>
+            <h3 className='mainTitle'>Invite a friend and get bonuses</h3>
             <InviteCard
                 icon1={inviteIcon}
                 title1="Invite friend"
@@ -19,21 +35,37 @@ export default function Invite() {
                 title2="Invite with Telegram Premium"
                 subtitle2="&#128176; 50,000 for you & your friend"
             />
-            <div className='clipBoard'>
-                <h3 className='clipBoard__txt'>Invite Friend</h3>
-                <div className='clipBoard__img'>
-                    <img src="/copy.png" alt="copyboard" className='copy__icon'/>
+            <div className="invite-button-container">
+                <div className="invite-button">
+                    <span>Invite Friend</span>
+                </div>
+                <div className="copy-icon" onClick={handleCopyClick}>
+                    <img src="/copy.png" alt="copyboard" className='copy__icon' />
                 </div>
             </div>
+
+            <Tasks
+                svgIcon1={telegramIcon}
+                title="Telegram Mini-Bonus"
+                subtitle="Invite with Telegram premium to double your earning by 50%"
+            />
+            <Tasks
+                svgIcon1={telegramIcon}
+                title="Telegram Super-Bonus"
+                subtitle="Invite with Telegram premium to double your earning by 100%"
+            />
+
             <div className='footer__container'>
 
-                <div className='footer__content'>
-                    <svg className='footer__svg__size' viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <g id="SVGRepo_bgCarrier" stroke-width="0" />
-                        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" />
-                        <g id="SVGRepo_iconCarrier"> <path opacity="0.5" d="M2 12.2039C2 9.91549 2 8.77128 2.5192 7.82274C3.0384 6.87421 3.98695 6.28551 5.88403 5.10813L7.88403 3.86687C9.88939 2.62229 10.8921 2 12 2C13.1079 2 14.1106 2.62229 16.116 3.86687L18.116 5.10812C20.0131 6.28551 20.9616 6.87421 21.4808 7.82274C22 8.77128 22 9.91549 22 12.2039V13.725C22 17.6258 22 19.5763 20.8284 20.7881C19.6569 22 17.7712 22 14 22H10C6.22876 22 4.34315 22 3.17157 20.7881C2 19.5763 2 17.6258 2 13.725V12.2039Z" stroke="#85827d" stroke-width="1.5" /> <path d="M9 16C9.85038 16.6303 10.8846 17 12 17C13.1154 17 14.1496 16.6303 15 16" stroke="#85827d" stroke-width="1.5" stroke-linecap="round" /> </g>
-                    </svg>
-                    <p className='footer__text'>Home</p>
+                <div className='footer__contentII'>
+                    <Link to='/home'>
+                        <svg className='footer__svg__size' viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <g id="SVGRepo_bgCarrier" stroke-width="0" />
+                            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" />
+                            <g id="SVGRepo_iconCarrier"> <path opacity="0.5" d="M2 12.2039C2 9.91549 2 8.77128 2.5192 7.82274C3.0384 6.87421 3.98695 6.28551 5.88403 5.10813L7.88403 3.86687C9.88939 2.62229 10.8921 2 12 2C13.1079 2 14.1106 2.62229 16.116 3.86687L18.116 5.10812C20.0131 6.28551 20.9616 6.87421 21.4808 7.82274C22 8.77128 22 9.91549 22 12.2039V13.725C22 17.6258 22 19.5763 20.8284 20.7881C19.6569 22 17.7712 22 14 22H10C6.22876 22 4.34315 22 3.17157 20.7881C2 19.5763 2 17.6258 2 13.725V12.2039Z" stroke="#85827d" stroke-width="1.5" /> <path d="M9 16C9.85038 16.6303 10.8846 17 12 17C13.1154 17 14.1496 16.6303 15 16" stroke="#85827d" stroke-width="1.5" stroke-linecap="round" /> </g>
+                        </svg>
+                        <p className='footer__text'>Home</p>
+                    </Link>
                 </div>
 
                 <div className='footer__contentII'>
@@ -48,7 +80,7 @@ export default function Invite() {
                     </Link>
                 </div>
 
-                <div className='footer__contentII'>
+                <div className='footer__content'>
                     <svg className='footer__svg__size' fill="#85827d" viewBox="0 -64 640 640" xmlns="http://www.w3.org/2000/svg">
                         <g id="SVGRepo_bgCarrier" stroke-width="0" />
                         <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" />
